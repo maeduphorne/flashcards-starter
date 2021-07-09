2bb51455481c46a16a4ce876c952691c2400435d
Mae Duphorne <maelduphorne@gmail.com>
2021-07-08 19:51:28 -0600

    Clean up using linter

--- src/Turn.js
+++ src/Turn.js
@@ -1,32 +1,32 @@
 class Turn {
- constructor(guess, card){
-   this.guess = guess;
-   this.card = card;
- };
+  constructor(guess, card) {
+    this.guess = guess;
+    this.card = card;
+  }
 
- returnGuess() {
-   return this.guess;
- };
+  returnGuess() {
+    return this.guess;
+  }
 
- returnCard() {
-   return this.card;
- };
+  returnCard() {
+    return this.card;
+  }
 
- evaluateGuess() {
-   if(this.card.correctAnswer === this.guess) {
-     return true;
-   } else {
-     return false;
-   };
- };
+  evaluateGuess() {
+    if (this.card.correctAnswer === this.guess) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
- giveFeedback() {
-   if(this.evaluateGuess()) {
-     return 'Correct!'
-   } else {
-     return 'Incorrect!'
-   };
- };
-};
+  giveFeedback() {
+    if (this.evaluateGuess()) {
+      return 'Correct!'
+    } else {
+      return 'Incorrect!'
+    }
+  }
+}
 
 module.exports = Turn;
